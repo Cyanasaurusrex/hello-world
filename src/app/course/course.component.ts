e1c384f8fad9fe75754d213c49042847b7f90633
Nathan Moon <moonman700@gmail.com>
2023-09-17 20:53:34 -0400

    Added API call to submit button

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http'; // Import HttpClient
 
 @Component({
   selector: 'app-course',
@@ -7,9 +8,22 @@ import { Component } from '@angular/core';
 })
 export class CourseComponent {
   inputText: string = '';
+  apiUrl: string = 'https://api.openweathermap.org/data/2.5/weather?q={{inputPlaceholder}}&appid={{apiKey}}&units=imperial'
+  apiKey: string = ''
+
+  constructor(private http: HttpClient) {} // Inject HttpClient
 
   sendData() {
-    console.log(this.inputText)
+    const cityUrl = this.apiUrl.replace('{{inputPlaceholder}}', this.inputText)
+    const dynamicUrl = cityUrl.replace('{{apiKey}}', this.apiKey)
+    this.http.get(dynamicUrl).subscribe(
+      (data) => {
+        console.log('API Response:', data);
+      },
+      (error) => {
+        // Handle any errors that occurred during the API request
+        console.error('An error occurred:', error);
+      }
+    );
   }
-
 }
