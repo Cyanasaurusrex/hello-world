8a4ca0b8dee67dc0c2dde5cb7568a27f21149d06
Nathan Moon <moonman700@gmail.com>
2023-09-20 22:51:50 -0400

    Fixed problem with both API calls and added local storage

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { instantForecast } from './instantForecast.interface';
 import { WeatherData } from './forecast.interface';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-course',
@@ -12,9 +13,12 @@ export class CourseComponent {
   inputText: string = '';
   apiUrl: string = 'https://api.openweathermap.org/data/2.5/weather?q={{inputPlaceholder}}&appid={{apiKey}}&units=imperial'
   forecastUrl: string = 'https://api.openweathermap.org/data/2.5/forecast?q={{inputPlaceholder}}&appid={{apiKey}}&units=imperial'
-  apiKey: string = '21d11b007ade27ccee285312cfe144b9'
+  apiKey: string = ''
   instantForecast!: instantForecast;
   WeatherData!: WeatherData
+  dayArray: string[] = []
+  tempArray!: [number];
+  myArray = [1, 2, 3, 4, 5]
   
 
   constructor(private http: HttpClient) {} // Inject HttpClient
@@ -33,25 +37,34 @@ export class CourseComponent {
   sendData() {
     let cityUrl = this.apiUrl.replace('{{inputPlaceholder}}', this.inputText)
     let dynamicUrl = cityUrl.replace('{{apiKey}}', this.apiKey)
-    this.http.get<instantForecast>(dynamicUrl).subscribe(
-      (data) => {
+    this.http.get<instantForecast>(dynamicUrl).pipe(
+      switchMap((data) => {
         this.instantForecast = data
-      },
-      (error) => {
-        // Handle any errors that occurred during the API request
-        console.error('An error occurred:', error);
-      }
-    );
-    cityUrl = this.forecastUrl.replace('{{inputPlaceholder}}', this.inputText)
-    dynamicUrl = cityUrl.replace('{{apiKey}}', this.apiKey)
-    this.http.get<WeatherData>(dynamicUrl).subscribe(
+        console.log(this.instantForecast)
+        cityUrl = this.forecastUrl.replace('{{inputPlaceholder}}', this.inputText)
+        dynamicUrl = cityUrl.replace('{{apiKey}}', this.apiKey)
+        return this.http.get<WeatherData>(dynamicUrl)
+      })
+    ).subscribe(
       (data) => {
-        this.WeatherData = data
-        console.log(this.WeatherData)
+        this.WeatherData = data;
+        
+        // finds the next 5 days and stores then in dayArray
+        for (let i=0; i < 40; i++) {
+          if (!this.dayArray.includes(this.WeatherData.list[i].dt_txt.slice(0, 10))){
+            this.dayArray.push(this.WeatherData.list[i].dt_txt.slice(0, 10))
+          }
+        }
+
+        for (let i=0; i < this.dayArray.length; i++) {
+          
+        }
+
       },
       (error) => {
         console.error('An error occurred:', error)
       }
     )
+    localStorage.setItem('myArray', JSON.stringify(this.myArray))  
   }
 }
